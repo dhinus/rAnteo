b53a073bb824ba45d69dfbd1556cc7e931185aa4
Francesco Negri <francesco.negri@cayenne.it>
2012-06-04 12:12:35 +0200

    fixed 2010 version (better late than never!)
    * removed php notices
    * retabbed with white space
    * fixed small typos in json database

--- conservatorio.js
+++ conservatorio.js
@@ -1,141 +1,71 @@
 conservatorio = [
-
 {day: 'Mercoledì 30 Giugno'},
-
 {day: 'Giovedì 1 Luglio', concert: 'Teatro-Musica 1', timeConcert: '21.00', title: 'LA PRIMA COSA BELLA', timeFilm: '22.00', director: 'P. Virzì', actors: 'V. Mastandrea, M. Ramazzotti', country: 'Italia', length: '116'},
-
 {day: 'Venerdì 2 Luglio', concert: 'Le 6 corde', timeConcert: '21.00', title: 'I LOVE RADIO ROCK', timeFilm: '22.00', director: 'R. Curtis', actors: 'P. S. Hoffman, K.Branagh', country: 'Gran Bretagna', length: '129'},
-
 {day: 'Sabato 3 Luglio', concert: 'La danza e la paura', timeConcert: '21.00', title: 'SHUTTER IS LAND', timeFilm: '22.00', director: 'M. Scorsese', actors: 'L. DiCaprio, M. Ruffalo', country: 'USA', length: '138'},
-
 {day: 'Domenica 4 Luglio', concert: 'Tentazioni e occasioni', timeConcert: '21.00', title: 'IL CONCERTO', timeFilm: '22.00', director: 'R. Mihaileanu', actors: 'A. Guskov, M. Laurent', country: 'Romania', length: '120'},
-
 {day: 'Lunedì 5 Luglio', concert: 'Woody e il quartetto Jazz', timeConcert: '21.00', title: 'BASTA CHE FUNZIONI', timeFilm: '22.00', director: 'W. Allen', actors: 'L. David, E. R. Wood', country: 'Francia, USA', length: '96'},
-
 {day: 'Martedì 6 Luglio', concert: 'Musical', timeConcert: '21.00', title: 'NINE', timeFilm: '22.00', director: 'R. Marshall', actors: 'D. Day-Lewis, P. Cruz', country: 'USA', length: '110'},
-
 {day: 'Mercoledì 7 Luglio', concert: 'Love songs', timeConcert: '21.00', title: '500 GIORNI INSIEME', timeFilm: '22.00', director: 'M. Webb', actors: 'J. Gordon-Levitt, Z. Deschanel', country: 'USA', length: '95'},
-
 {day: 'Giovedì 8 Luglio', concert: 'Luci e Ombre', timeConcert: '21.00', title: 'L\'UOMO NELL\'OMBRA', timeFilm: '22.00', director: 'R. Polanski', actors: 'E. McGregor, P. Brosnan', country: 'Germania, USA', length: '131'},
-
 {day: 'Venerdì 9 Luglio', concert: 'Teatro-Musica 2', timeConcert: '21.00', title: 'HAPPY FAMILY', timeFilm: '22.00', director: 'G. Salvatores', actors: 'F. De Luigi, D. Abatantuono', country: 'Italia', length: '90'},
-
 {day: 'Sabato 10 Luglio', concert: 'France', timeConcert: '21.00', title: 'IL PICCOLO NICOLAS E I SUOI GENITORI', timeFilm: '22.00', director: 'L. Tirard', actors: 'M. Godart, K. Merad', country: 'Francia', length: '90'},
-
 {day: 'Domenica 11 Luglio', concert: 'Il quartetto di arpe', timeConcert: '21.00', title: 'BRIGHT STAR', timeFilm: '22.00', director: 'J. Campion', actors: 'B. Whishaw, A. Cornish', country: 'Gran Bretagna', length: '120'},
-
 {day: 'Lunedì 12 Luglio', concert: 'Sei Soli', timeConcert: '21.00', title: 'A SINGLE MAN', timeFilm: '22.00', director: 'T. Ford', actors: 'C. Firth, J. Moore', country: 'USA', length: '95'},
-
 {day: 'Martedì 13 Luglio', concert: 'Lo scambio delle coppie', timeConcert: '21.00', title: 'LA NOSTRA VITA', timeFilm: '22.00', director: 'D. Luchetti', actors: 'E. Germano, R. Bova', country: 'Italia', length: '100'},
-
 {day: 'Mercoledì 14 Luglio', concert: 'America', timeConcert: '21.00', title: 'MOTEL WOODS TOCK', timeFilm: '22.00', director: 'A. Lee', actors: 'D. Martin, K. Chamberlin', country: 'USA', length: '121'},
-
 {day: 'Giovedì 15 Luglio', concert: 'Teatro-Musica 3', timeConcert: '21.00', title: 'IL SEGRETO DEI SUOI OCCHI', timeFilm: '22.00', director: 'J. J. Campanella', actors: 'R. Darín, S. Villamil', country: 'Spagna, Argentina', length: '129'},
-
 {day: 'Venerdì 16 Luglio', concert: 'Sax and Sex', timeConcert: '21.00', title: 'MINE VAGANTI', timeFilm: '22.00', director: 'F. Ozpetek', actors: 'R. Scamarcio, N. Grimaudo', country: 'Italia', length: '110'},
-
 {day: 'Sabato 17 Luglio', concert: 'Film muto con concerto dal vivo', timeConcert: '22.00', title: 'LA CADUTA DELLA CASA USHER', timeFilm: '22.00', director: 'J. Epstein', actors: 'J. Debucourt, M. Gance', country: 'Francia', length: '61'},
-
 {day: 'Domenica 18 Luglio', concert: 'Fiore di gemko, fior di betulla, fiori d\'amore', timeConcert: '21.00', title: 'DIECI INVERNI', timeFilm: '22.00', director: 'V. Mieli', actors: 'I. Ragonese, M. Riondino', country: 'Italia', length: '99'},
-
 {day: 'Lunedì 19 Luglio', concert: 'Sax four for fun', timeConcert: '21.00', title: 'TRA LE NUVOLE', timeFilm: '22.00', director: 'J. Reitman', actors: 'G. Clooney, A. Kendrick', country: 'USA', length: '108'},
-
 {day: 'Martedì 20 Luglio', concert: 'Tetralumina', timeConcert: '21.00', title: 'LE QUATTRO VOLTE', timeFilm: '22.00', director: 'M. Frammartino', actors: 'G. Fuda, B. Timpano', country: 'Italia, Germania, Svizzera', length: '88'},
-
 {day: 'Mercoledì 21 Luglio', concert: 'Solisti', timeConcert: '21.00', title: 'IL SOLISTA', timeFilm: '22.00', director: 'J. Wright', actors: 'R. Downey Jr., J. Foxx', country: 'USA', length: '117'},
-
 {day: 'Giovedì 22 Luglio', concert: 'Cherie', timeConcert: '21.00', title: 'CHERI', timeFilm: '22.00', director: 'S. Frears', actors: 'M. Pfeiffer, K. Bates', country: 'Gran Bretagna', length: '100'},
-
 {day: 'Venerdì 23 Luglio', concert: 'Classici', timeConcert: '21.00', title: 'IO SONO L\'AMORE', timeFilm: '22.00', director: 'L. Guadagnino', actors: 'T. Swinton, P. Delbono', country: 'Italia', length: '120'},
-
 {day: 'Sabato 24 Luglio', concert: 'Il fascino orientale', timeConcert: '21.00', title: 'I GATTI PERSIANI', timeFilm: '22.00', director: 'B. Ghobadi', actors: 'H. Behdad, N. Shaghaghi', country: 'Iran', length: '101'},
-
 {day: 'Domenica 25 Luglio', concert: 'L\'eleganza e il sorriso', timeConcert: '21.00', title: 'IL RICCIO', timeFilm: '22.00', director: 'M. Achache', actors: 'J. Balasko, G. Le Guillermic', country: 'Francia, Italia', length: '100'},
-
 {day: 'Lunedì 26 Luglio', concert: 'Un abbraccio di Cecilia', timeConcert: '21.00', title: 'GLI ABBRACCI SPEZZATI', timeFilm: '22.00', director: 'P. Almodóvar', actors: 'P. Cruz, L. Homar', country: 'Spagna', length: '129'},
-
 {day: 'Martedì 27 Luglio', concert: 'Video Computer Art', timeConcert: '21.00', title: 'AVATAR', timeFilm: '22.00', director: 'J. Cameron', actors: 'S. Worthington, Z. Saldana', country: 'USA', length: '162'},
-
 {day: 'Mercoledì 28 Luglio', concert: 'Violenza e tensione', timeConcert: '21.00', title: 'IL PROFETA', timeFilm: '22.00 ', director: 'J. Audiard', actors: 'T. Rahim, N. Arestrup', country: 'Francia', length: '149'},
-
 {day: 'Giovedì 29 Luglio', concert: 'AsaNisiMasa', timeConcert: '21.00', title: 'A SERIOUS MAN', timeFilm: '22.00', director: 'E. e J. Coen', actors: 'M. Stuhlbarg, R. Kind', country: 'USA', length: '105'},
-
 {day: 'Venerdì 30 Luglio', concert: 'anime e speranze', timeConcert: '21.00', title: 'THE ROAD', timeFilm: '22.00', director: 'J. Hillcoat', actors: 'V. Mortensen, K. Smit-McPhee', country: 'USA', length: '112'},
-
 {day: 'Sabato 31 Luglio', concert: 'Epos', timeConcert: '21.00', title: 'BAARÌA', timeFilm: '22.00', director: 'G. Tornatore', actors: 'F. Scianna, M. Madè', country: 'Italia, Francia', length: '150'},
-
 {day: 'Domenica 1 Agosto', timeFilm: '21.30', title: 'IO, LORO E LARA', director: 'C. Verdone', actors: 'C. Verdone, L. Chiatti', country: 'Italia', length: '115'},
-
 {day: 'Lunedì 2 Agosto', timeFilm: '21.30', title: 'È COMPLICATO', director: 'N. Meyers', actors: 'M. Streep, A. Baldwin', country: 'USA', length: '118'},
-
 {day: 'Martedì 3 Agosto', timeFilm: '21.30', title: 'WELCOME', director: 'P. Lioret', actors: 'V. Lindon, F. Ayverdi', country: 'Francia', length: '110'},
-
 {day: 'Mercoledì 4 Agosto', timeFilm: '21.30', title: 'L\'UOMO CHE VERRÀ', director: 'G. Diritti', actors: 'A. Rohrwacher, M. Sansa', country: 'Italia', length: '117'},
-
 {day: 'Giovedì 5 Agosto', timeFilm: '21.30', title: 'COSA VOGLIO DI PIÙ', director: 'S. Soldini', actors: 'A. Rohrwacher, P. Favino', country: 'Italia,Svizzera', length: '126'},
-
 {day: 'Venerdì 6 Agosto', timeFilm: '21.30', title: 'JULIE & JULIA', director: 'N. Ephron', actors: 'M. Streep, A. Adams', country: 'USA', length: '123'},
-
 {day: 'Sabato 7 Agosto', timeFilm: '21.30', title: 'BASILICATA COAST TO COAST', director: 'R. Papaleo', actors: 'G. Mezzogiorno, A. Gassman', country: 'Italia', length: '105'},
-
 {day: 'Domenica 8 Agosto', timeFilm: '21.30', title: 'IL GRANDE SOGNO', director: 'M. Placido', actors: 'R. Scamarcio, J. Trinca', country: 'Italia', length: '101'},
-
 {day: 'Lunedì 9 Agosto', timeFilm: '21.30', title: 'AN EDUCATION', director: 'L. Scherfig', actors: 'P. Sarsgaard, C. Mulligan', country: 'Gran Bretagna', length: '100'},
-
 {day: 'Martedì 10 Agosto', timeFilm: '21.30', title: 'DRAQUILA', director: 'S. Guzzanti', actors: 'S. Guzzanti', country: 'Italia', length: '93'},
-
 {day: 'Mercoledì 11 Agosto', timeFilm: '21.30', title: 'THE INFORMANT!', director: 'S. Soderbergh', actors: 'M. Damon, M. Lynskey', country: 'USA', length: '108'},
-
 {day: 'Giovedì 12 Agosto', timeFilm: '21.30', title: 'CHE FINE HANNO FATTO I MORGAN?', director: 'M. Lawrence', actors: 'S. J. Parker, H. Grant', country: 'USA', length: '100'},
-
 {day: 'Venerdì 13 Agosto', timeFilm: '21.30', title: '2012', director: 'R. Emmerich', actors: 'J. Cusack, C. Ejiofor', country: 'USA', length: '158'},
-
 {day: 'Sabato 14 Agosto', timeFilm: '21.30', title: 'SEX AND THE CITY 2', director: 'M. P. king', actors: 'S. J. Parker, K. Cattrall', country: 'USA', length: '146'},
-
 {day: 'Domenica 15 Agosto', timeFilm: '21.30', title: 'BACIAMI ANCORA', director: 'G. Muccino', actors: 'S. Accorsi, V. Puccini', country: 'Italia', length: '139'},
-
 {day: 'Domenica 15 Agosto '},
-
 {day: 'Lunedì 16 Agosto', timeFilm: '21.30', title: 'L\'UOMO CHE FISSA LE CAPRE', director: 'G. Heslov', actors: 'E. McGregor, G. Clooney', country: 'Gran Bretagna, USA', length: '93'},
-
 {day: 'Martedì 17 Agosto', timeFilm: '21.30', title: 'GENITORI & FIGLI Agitare bene prima dell\'uso', director: 'G. Veronesi', actors: 'M. Placido, L. Littizzetto', country: 'Italia', length: '110'},
-
 {day: 'Mercoledì 18 Agosto', timeFilm: '21.30', title: 'AMABILIRESTI', director: 'P. Jackson', actors: 'M. Wahlberg, R. Weisz', country: 'Gran Bretagna', length: '139'},
-
 {day: 'Giovedì 19 Agosto', timeFilm: '21.30', title: 'COLPO DI FULMINE - Il mago della truffa', director: 'G. Ficarra, J. Requa', actors: 'J. Carrey, E. McGregor', country: 'USA', length: '97'},
-
 {day: 'Venerdì 20 Agosto', timeFilm: '21.30', title: 'SEGRETI DI FAMIGLIA', director: 'F. F. Coppola', actors: 'V. Gallo, M. Verdú', country: 'Argentina, USA', length: '127'},
-
 {day: 'Sabato 21 Agosto', timeFilm: '21.30', title: 'LO SPAZIO BIANCO', director: 'F. Comencini', actors: 'M. Buy, S. Cantalupo', country: 'Italia', length: '96'},
-
 {day: 'Domenica 22 Agosto', timeFilm: '21.30', title: 'SHERLOCK HOLMES', director: 'G. Ritchie', actors: 'R. Downey Jr., J. Law', country: 'Gran Bretagna', length: '134'},
-
 {day: 'Lunedì 23 Agosto', timeFilm: '21.30', title: 'BROTHERS', director: 'J. Sheridan', actors: 'J. Gyllenhaal, N. Portman', country: 'USA', length: '108'},
-
 {day: 'Martedì 24 Agosto', timeFilm: '21.30', title: 'AGORA', director: 'A. Amenábar', actors: 'R. Weisz, M. Minghella', country: 'Spagna, USA', length: '126'},
-
 {day: 'Mercoledì 25 Agosto', timeFilm: '21.30', title: 'BASTARDI SENZA GLORIA', director: 'Q. Tarantino', actors: 'B. Pitt, M. Laurent', country: 'Francia, Germania, USA', length: '153'},
-
 {day: 'Giovedì 26 Agosto', concert: 'Improvvisazione', timeConcert: '20.30', title: 'IL MIO AMICO ERIC', timeFilm: '21.30', director: 'K. Loach', actors: 'S. Evets, E. Cantona', country: 'Francia, Gran Bretagna', length: '116'},
-
 {day: 'Venerdì 27 Agosto', concert: 'Inquietudini e visioni', timeConcert: '20.30', title: 'MATRIMONI E ALTRI DISASTRI', timeFilm: '21.30', director: 'N. Di Majo', actors: 'F. Volo, M. Buy', country: 'Italia', length: '102'},
-
 {day: 'Sabato 28 Agosto', concert: 'luci ingrate, di spietate', timeConcert: '20.30', title: 'SUL MARE', timeFilm: '21.30', director: 'A. D\'Alatri', actors: 'D. Castiglio, M. Codecasa', country: 'Italia', length: '100'},
-
 {day: 'Domenica 29 Agosto', concert: 'humor - poesia - azzardo', timeConcert: '20.30', title: 'SOUL KITCHEN', timeFilm: '21.30', director: 'F. Akin', actors: 'A. Bousdoukos, M. Bleibtreu', country: 'Germania', length: '99'},
-
 {day: 'Lunedì 30 Agosto', concert: 'quartetto d\'archi', timeConcert: '20.30', title: 'IL CONCERTO', timeFilm: '21.30', director: 'R. Mihaileanu', actors: 'A. Guskov, M. Laurent', country: 'Romania', length: '120'},
-
 {day: 'Martedì 31 Agosto', concert: 'Canti da Harlem', timeConcert: '20.30', title: 'INVICTUS', timeFilm: '21.30 ', director: 'C. Eastwood', actors: 'M. Damon, M. Freeman', country: 'USA', length: '134'},
-
 {day: 'Mercoledì 1 Settembre', concert: 'La lotta e gli ideali', timeConcert: '20.30', title: 'ROBIN HOOD', timeFilm: '21.30 ', director: 'R. Scott', actors: 'R. Crowe, C. Blanchett', country: 'USA', length: '138'},
-
 {day: 'Giovedì 2 Settembre', concert: 'gradus ad Parnassus', timeConcert: '20.30', title: 'PARNASSUS L\'uomo che voleva ingannare il diavolo', timeFilm: '21.30', director: 'T. Gilliam', actors: 'H. Ledger, J. Depp', country: 'Canada, Francia, Gran Bretagna', length: '122'},
-
 {day: 'Venerdì 3 Settembre', concert: 'Il soffio divino', timeConcert: '20.30', title: 'IL NASTRO BIANCO', timeFilm: '21.30', director: 'M. Haneke', actors: 'S. Lothar, U. Tukur', country: 'Austria, Germania', length: '144'},
-
 {day: 'Sabato 4 Settembre', concert: 'L\'oriente rosso', timeConcert: '20.30', title: 'DEPARTURES', timeFilm: '21.30', director: 'Y. Takita', actors: 'M. Motoki, T. Yamazaki', country: 'Giappone', length: '130'},
-
 {day: 'Domenica 5 Settembre', concert: 'Fantastico mondo', timeConcert: '20.30', title: 'ALICE IN WONDERLAND', timeFilm: '21.30', director: 'T. Burton', actors: 'M. Wasikowska, J. Depp', country: 'USA', length: '108'}
-
 ]
